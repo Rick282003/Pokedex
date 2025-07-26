a0fa5873b9fe924cde6edbb98c0824eb274226c1
Rick282003 <richardgalo2003@gmail.com>
2025-07-25 19:50:45 -0600

    Deployment Attempt

--- vite.config.js
+++ vite.config.js
@@ -1,21 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
-import { viteStaticCopy } from 'vite-plugin-static-copy'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/Pokedex/',
+  base: process.env.DEPLOY_BASE_URL,
   plugins: [
     react(),
     tailwindcss(),
-    viteStaticCopy({
-      targets: [
-        {
-          src: 'public/404.html',
-          dest: '.'
-        }
-      ]
-    })
   ],
 })
